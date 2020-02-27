f70fbab4235b836a0d77ae92d4103effd80fbbfa
vyacheslav <svetlakoff88@gmail.com>
2020-02-27 12:31:19 +0300

    update

--- variables.py
+++ variables.py
@@ -3,7 +3,7 @@
 
 DROPBOX_TOKEN = ''
 
-TOKEN = ''
+TOKEN = ''   # telegram_token
 
 disciplines = [
     'Инфокоммуникационные системы и сети',
@@ -19,23 +19,23 @@ disciplines = [
     'Практика'
 ]
 
-info_com_sys_net = 'https://www.dropbox.com/sh/81qv2gkuka8fne7/AACWBUn24zU5tHsEmFamWJ_Sa?dl=0'
-big_data = 'https://www.dropbox.com/sh/h8d64yox98f19rg/AABb96GlwVQ2GGueGvOr_vNga?dl=0'
-modeling_system = 'https://www.dropbox.com/sh/6t40s3nmgf0r51r/AABU5AgZ_w9zoMwR7d-Xe3KQa?dl=0'
-graph_3d = 'https://www.dropbox.com/sh/9zlxmimvr79ks7m/AAB0N7z5x-pSwrkPK1yJV-m_a?dl=0'
-mispisit = 'https://www.dropbox.com/sh/f7znsyjccxna8rs/AABRIBOOnU_oodqEY_O3qOsla?dl=0'
-time_managment = 'https://www.dropbox.com/sh/5dtztw2ceqhzx06/AACBceRZ0Ts2EOU6AnAJv1VEa?dl=0'
-mobile_device_dev_tech = 'https://www.dropbox.com/sh/m9v9g6cgs97x669/AACGws4Powkj1MtxRe0eN7Rva?dl=0'
-mob_dev_net_comp_framework = 'https://www.dropbox.com/sh/5acc1ywp0ljw0q7/AACmCS--ElcRFjWqtBa2PqxRa?dl=0'
-osn_isp_1c_predp = 'https://www.dropbox.com/sh/1fu8ys5vbgf27qn/AAC22e57yp19PvORpTgYcAQqa?dl=0'
-prog_app_sred_inf_bez = 'https://www.dropbox.com/sh/wd6dpwhuhxovkhz/AABMaBTlsza31zYth-fq8Jo_a?dl=0'
-practice = 'https://www.dropbox.com/sh/k2g5orrvqbiatba/AAB_KEUYQ0M_RZfzv7TY5_v_a?dl=0'
+info_com_sys_net = ''  # path_to_remote_directory
+big_data = ''    # path_to_remote_directory
+modeling_system = ''     # path_to_remote_directory
+graph_3d = ''  # path_to_remote_directory
+mispisit = ''     # path_to_remote_directory
+time_managment = ''      # path_to_remote_directory
+mobile_device_dev_tech = ''        # path_to_remote_directory
+mob_dev_net_comp_framework = ''        # path_to_remote_directory
+osn_isp_1c_predp = ''      # path_to_remote_directory
+prog_app_sred_inf_bez = ''    # path_to_remote_directory
+practice = ''      # path_to_remote_directory
 
 
-schedule_link = "https://www.dropbox.com/s/keq6jdyu1lwtwqc/schedule_2020.xlsx?dl=0"
-schedule_source = "/home/vyacheslav/Projects/active_project's/TelegramBot/resources/schedule.html"
-schedule_source_home = "/home/vyacheslav/Projects/active_project's/TelegramBot/resources/schedule_2020.xlsx"
-browser_driver = "/usr/local/chromedriver_linux64/chromedriver"
+schedule_link = ""
+schedule_source = ""
+schedule_source_home = ""
+browser_driver = ""
 day_obj = ''
 
 user_agent = {
@@ -44,12 +44,12 @@ user_agent = {
 
 teacher_email = {
     'Максимов К.В.': '',
-    'Гавриленко А.В.': 'agavr-51@yandex.ru ',
+    'Гавриленко А.В.': '',
     'Ребус Н.А.': '',
     'Кудинов А.А.': '',
     'Прокимнов Н.Н': '',
     'Дьяконова И.А': '',
     'Ожередов В.А.': '',
-    'Гнибеда А.Ю.': 'artem_gnibeda@rambler.ru',
+    'Гнибеда А.Ю.': '',
     'Смелкова И.А.': ''
 }
